c13f8dc18af0bad2c24f3320d86b970ac797a8d6
nkgvl <je1gvl@gmail.com>
2023-10-12 18:37:43 +0100

    Remove bouncing_ball submodule

--- misc.py
+++ misc.py
@@ -2,5 +2,5 @@ def compute_conv2d_output_size(input_size, kernel_size, stride, padding):
     h, w = input_size
     h_out = (h - kernel_size + 2 * padding) // stride + 1
     w_out = (w - kernel_size + 2 * padding) // stride + 1
-    
+
     return h_out, w_out
--- vae.py
+++ vae.py
@@ -1,25 +1,43 @@
 import torch
+import torch.distributions as D
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.distributions as D
+
 from misc import compute_conv2d_output_size
 
 
 class Encoder(nn.Module):
     def __init__(self, image_size, image_channels, a_dim):
-        
         super(Encoder, self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels=image_channels, out_channels=32, kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1)
-        
+
+        self.conv1 = nn.Conv2d(
+            in_channels=image_channels,
+            out_channels=32,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1
+        )
+        self.conv3 = nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1
+        )
+
         conv_output_size = image_size
         for _ in range(3):
-            conv_output_size = compute_conv2d_output_size(conv_output_size, kernel_size=3, stride=2, padding=1)
-            
-        self.fc_mean = nn.Linear(in_features=32*conv_output_size[0]*conv_output_size[1], out_features=a_dim)
-        self.fc_std = nn.Linear(in_features=32*conv_output_size[0]*conv_output_size[1], out_features=a_dim)
+            conv_output_size = compute_conv2d_output_size(
+                conv_output_size, kernel_size=3, stride=2, padding=1
+            )
+
+        self.fc_mean = nn.Linear(
+            in_features=32 * conv_output_size[0] * conv_output_size[1],
+            out_features=a_dim,
+        )
+        self.fc_std = nn.Linear(
+            in_features=32 * conv_output_size[0] * conv_output_size[1],
+            out_features=a_dim,
+        )
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -28,11 +46,12 @@ class Encoder(nn.Module):
         # print("conv2:", x.shape)
         x = F.relu(self.conv3(x))
         # print("conv3:", x.shape)
-        
+
         x_mean = self.fc_mean(x.view(x.shape[0], -1))
         x_std = F.softplus(self.fc_std(x.view(x.shape[0], -1)))
-        
+
         return D.Normal(loc=x_mean, scale=x_std)
+
 
 class BernoulliDecoder(nn.Module):
     def __init__(self, a_dim, image_size, image_channels):
@@ -40,13 +59,39 @@ class BernoulliDecoder(nn.Module):
 
         conv_output_size = image_size
         for _ in range(3):
-            conv_output_size = compute_conv2d_output_size(conv_output_size, kernel_size=3, stride=2, padding=1)
+            conv_output_size = compute_conv2d_output_size(
+                conv_output_size, kernel_size=3, stride=2, padding=1
+            )
         self.conv_output_size = conv_output_size
 
-        self.fc = nn.Linear(in_features=a_dim, out_features=32*conv_output_size[0]*conv_output_size[1])
-        self.deconv1 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.deconv2 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.deconv3 = nn.ConvTranspose2d(in_channels=32, out_channels=image_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.fc = nn.Linear(
+            in_features=a_dim,
+            out_features=32 * conv_output_size[0] * conv_output_size[1],
+        )
+        self.deconv1 = nn.ConvTranspose2d(
+            in_channels=32,
+            out_channels=32,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            output_padding=1,
+        )
+        self.deconv2 = nn.ConvTranspose2d(
+            in_channels=32,
+            out_channels=32,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            output_padding=1,
+        )
+        self.deconv3 = nn.ConvTranspose2d(
+            in_channels=32,
+            out_channels=image_channels,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            output_padding=1,
+        )
 
     def forward(self, x):
         x = F.relu(self.fc(x))
@@ -54,8 +99,9 @@ class BernoulliDecoder(nn.Module):
         x = F.relu(self.deconv1(x))
         x = F.relu(self.deconv2(x))
         x = self.deconv3(x)
-        
+
         return D.Bernoulli(logits=x)
+
 
 class GaussianDecoder(nn.Module):
     def __init__(self, a_dim, image_size, image_channels):
@@ -63,14 +109,47 @@ class GaussianDecoder(nn.Module):
 
         conv_output_size = image_size
         for _ in range(3):
-            conv_output_size = compute_conv2d_output_size(conv_output_size, kernel_size=3, stride=2, padding=1)
+            conv_output_size = compute_conv2d_output_size(
+                conv_output_size, kernel_size=3, stride=2, padding=1
+            )
         self.conv_output_size = conv_output_size
 
-        self.fc = nn.Linear(in_features=a_dim, out_features=32*conv_output_size[0]*conv_output_size[1])
-        self.deconv1 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.deconv2 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.deconv3_mean = nn.ConvTranspose2d(in_channels=32, out_channels=image_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.deconv3_std = nn.ConvTranspose2d(in_channels=32, out_channels=image_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.fc = nn.Linear(
+            in_features=a_dim,
+            out_features=32 * conv_output_size[0] * conv_output_size[1],
+        )
+        self.deconv1 = nn.ConvTranspose2d(
+            in_channels=32,
+            out_channels=32,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            output_padding=1,
+        )
+        self.deconv2 = nn.ConvTranspose2d(
+            in_channels=32,
+            out_channels=32,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            output_padding=1,
+        )
+        self.deconv3_mean = nn.ConvTranspose2d(
+            in_channels=32,
+            out_channels=image_channels,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            output_padding=1,
+        )
+        self.deconv3_std = nn.ConvTranspose2d(
+            in_channels=32,
+            out_channels=image_channels,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            output_padding=1,
+        )
 
     def forward(self, x):
         x = F.relu(self.fc(x))
@@ -79,5 +158,5 @@ class GaussianDecoder(nn.Module):
         x = F.relu(self.deconv2(x))
         x_mean = self.deconv3_mean(x)
         x_std = F.softplus(self.deconv3_std(x))
-        
+
         return D.Normal(loc=x_mean, scale=x_std)
