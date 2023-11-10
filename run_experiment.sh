874ee9ab3c0025be355b77a3cb8f34dfd1770081
Naoki Kiyohara <n.kiyohara23@imperial.ac.uk>
2023-11-10 20:34:31 +0000

    Add run_experiment.sh

---
+++ run_experiment.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Get current time and format it for experiment name
+current_time=$(date +"%Y%m%d_%H%M%S")
+experiment_name="experiment_$current_time"
+
+# Create a directory with the experiment name inside the checkpoints directory
+checkpoint_directory="checkpoints/$experiment_name"
+mkdir -p "$checkpoint_directory"
+
+# Run the Python script with all arguments explicitly set
+python train.py \
+    --batch_size 128 \
+    --z_dim 4 \
+    --a_dim 2 \
+    --K 3 \
+    --decoder_type "bernoulli" \
+    --reconst_weight 1.0 \
+    --regularization_weight 0.3 \
+    --symmetrize_covariance False \
+    --epochs 80 \
+    --learning_rate 0.007 \
+    --learning_rate_decay 0.8 \
+    --burn_in 3 \
+    --batch_operation "mean" \
+    --sequence_operation "mean" \
+    --scheduler_step 20 \
+    --device "cuda:0" \
+    --dtype "float64" \
+    --checkpoint_dir "$checkpoint_directory" \
+    --name "$experiment_name"
