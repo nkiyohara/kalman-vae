0ef8beb251766cc0d21dd7e745c2b11f306ed262
Naoki Kiyohara <n.kiyohara23@imperial.ac.uk>
2023-11-10 20:40:33 +0000

    Add comments in config.py

--- config.py
+++ config.py
@@ -2,23 +2,33 @@ from typing import Literal, NamedTuple
 
 
 class Config(NamedTuple):
-    device: str
-    batch_size: int
-    dtype: Literal["float", "double"]
-    epochs: int
-    learning_rate: float
-    learning_rate_decay: float
-    scheduler_step: int
-    a_dim: int
-    z_dim: int
-    K: int
-    decoder_type: Literal["bernoulli", "gaussian"]
-    data_root_dir: str
-    checkpoint_dir: str
-    project_name: str
-    reconst_weight: float
-    regularization_weight: float
-    symmetrize_covariance: bool
-    burn_in: int
-    batch_operation: Literal["mean", "sum"]
-    sequence_operation: Literal["mean", "sum"]
+    """Configuration for the experiment"""
+
+    # Environment Settings - Parameters related to the experimental environment
+    device: str  # Device to use for training
+    dtype: Literal["float", "double"]  # Data type for tensors
+    checkpoint_dir: str  # Directory to save checkpoints
+    project_name: str  # Name of the project
+    name: str  # Name of the experiment
+
+    # Data Settings - Parameters related to data
+    data_root_dir: str  # Root directory of the data
+    batch_size: int  # Batch size
+    batch_operation: Literal["mean", "sum"]  # Operation per batch
+    sequence_operation: Literal["mean", "sum"]  # Operation per sequence
+
+    # Model Settings - Parameters related to the configuration of the model
+    a_dim: int  # Dimension of encoded space a
+    z_dim: int  # Dimension of latent space z
+    K: int  # Number of matrices for weighted average in observation and transition matrices
+    decoder_type: Literal["bernoulli", "gaussian"]  # Type of decoder
+    reconst_weight: float  # Weight for reconstruction loss
+    regularization_weight: float  # Weight for regularization loss
+    symmetrize_covariance: bool  # Whether to symmetrize the covariance matrix
+
+    # Training Settings - Parameters related to training
+    epochs: int  # Number of training epochs
+    learning_rate: float  # Learning rate
+    learning_rate_decay: float  # Learning rate decay
+    scheduler_step: int  # Number of epochs between scheduler steps
+    burn_in: int  # Number of burn-in steps
